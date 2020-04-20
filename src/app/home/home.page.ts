b3b9bbeedcfa948f4d0ffaf5c951d258e3cb9438
prajakta-sontakke <prajakta.sontakke@hp.com>
2020-04-20 13:08:16 +0530

    login page n sliding

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
-import { NavController } from '@ionic/angular';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { NavController, IonSlides } from '@ionic/angular';
 import { NgbDropdownConfig } from '@ng-bootstrap/ng-bootstrap';
+import { OwlOptions } from 'ngx-owl-carousel-o';
+
 
 @Component({
   selector: 'app-home',
@@ -9,23 +11,58 @@ import { NgbDropdownConfig } from '@ng-bootstrap/ng-bootstrap';
   providers: [NgbDropdownConfig]
 })
 export class HomePage {
-  // slideOpts = {
-  //   initialSlide: 1,
-  //   // autoPlay: true,
-  //   slidesPerView: 1,
-  //   spaceBetween: 0,
-  //   centeredSlides: true,
-  //   speed: 400
-  // };
-  carouselOption1 = {
-    loop: true,
-    margin: 10,
-    items: 1,
-    nav: true,
+  slideOpts = {
+    initialSlide: 1,
     autoplay: true,
-    autoplayTimeout: 5500,
-    navText: ["<i class='ti-angle-left'></i>", "<i class='ti-angle-right'></i>"]
+    slidesPerView: 1,
+    spaceBetween: 0,
+    centeredSlides: true,
+    speed: 400
+  };
+ @ViewChild(IonSlides) slider: IonSlides;
+  // carouselOption1 = {
+  //   loop: true,
+  //   margin: 10,
+  //   items: 1,
+  //   rtl: true,
+  //   //slidesPerView: 1,
+  //   nav: true,
+  //   autoplay: true,
+  //   autoplayTimeout: 5500,
+  //   navText: ["<i class='ti-angle-left'></i>", "<i class='ti-angle-right'></i>"]
+  // }
+  
+  slidePrev() {
+    this.slider.slidePrev();
   }
+  slideNext() {
+    this.slider.slideNext();
+  }
+
+  // customOptions: OwlOptions = {
+  //   loop: true,
+  //   mouseDrag: false,
+  //   touchDrag: false,
+  //   pullDrag: false,
+  //   dots: false,
+  //   navSpeed: 700,
+  //   navText: ['Previous', 'Next'],
+  //   responsive: {
+  //     0: {
+  //       items: 1 
+  //     },
+  //     400: {
+  //       items: 2
+  //     },
+  //     740: {
+  //       items: 3
+  //     },
+  //     940: {
+  //       items: 4
+  //     }
+  //   },
+  //   nav: true
+  // }
   constructor(private navCtrl: NavController, config: NgbDropdownConfig) {
     config.placement = 'bottom-right';
   }
